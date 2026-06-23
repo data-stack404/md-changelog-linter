d73a153bf1231a076238e4d1e7192036c45244e0
data-stack404 <kevin@data-stack.eu>
2026-06-23 10:00:24 +0200

    deleted dist folder

--- dist/parser.d.ts
+++
@@ -1,3 +0,0 @@
-import { ParsedChangelog } from './types';
-export declare function parseChangelog(content: string): ParsedChangelog;
-//# sourceMappingURL=parser.d.ts.map
--- dist/rules.d.ts
+++
@@ -1,3 +0,0 @@
-import { ParsedChangelog, LintDiagnostic } from './types';
-export declare function runRules(changelog: ParsedChangelog): LintDiagnostic[];
-//# sourceMappingURL=rules.d.ts.map
